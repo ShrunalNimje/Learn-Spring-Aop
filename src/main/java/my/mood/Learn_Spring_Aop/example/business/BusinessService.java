e66f9d5d5801f73d6a5ec66c3ae6cad335d741ac
Shrunal Nimje <shrunalnimje@gmail.com>
2024-09-23 20:29:42 +0530

    Added new annotation

--- src/main/java/my/mood/Learn_Spring_Aop/example/business/BusinessService.java
+++ src/main/java/my/mood/Learn_Spring_Aop/example/business/BusinessService.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import my.mood.Learn_Spring_Aop.example.annotation.TrackTime;
 import my.mood.Learn_Spring_Aop.example.data.DataService;
 
 @Service
@@ -13,7 +14,9 @@ public class BusinessService {
 	@Autowired
 	private DataService dataService;
 	
+	@TrackTime
 	public int CalculateMax() {
+		
 		int [] max = dataService.RetrieveData();
 		// throw new RuntimeException("Something wrong going to happen!");
 		return Arrays.stream(max).max().orElse(0);
