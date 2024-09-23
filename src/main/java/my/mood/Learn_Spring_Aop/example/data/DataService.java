e9a98f0d5792f0616200414e3ae8ca4fe522954e
Shrunal Nimje <shrunalnimje@gmail.com>
2024-09-23 20:30:12 +0530

    changes in code

--- src/main/java/my/mood/Learn_Spring_Aop/example/data/DataService.java
+++ src/main/java/my/mood/Learn_Spring_Aop/example/data/DataService.java
@@ -6,6 +6,14 @@ import org.springframework.stereotype.Repository;
 public class DataService {
 
 	public int [] RetrieveData() {
+		
+		try {
+			Thread.sleep(30);
+		} 
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 		return new int [] {12, 23, 34, 45, 56, 67, 78, 89, 90};
 	}
 }
